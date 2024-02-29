74896605fde69f2e97221c4a3f89253e00d2efa2
Arham Natiq <arhamnatiq25@gmail.com>
2024-02-29 18:35:05 +0500

    backend folder and tailwind added

---
+++ backend/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="bg-purple-600">hello</h1>
+</template>
+
+
